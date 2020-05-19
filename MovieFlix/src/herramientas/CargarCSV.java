fcea272a7e462ca77103234f5acc9e520fbee612
dvigon <davidvigon@hotmail.es>
2020-05-19 17:37:32 +0200

    Añadido el String ArchivoCSV que almacenará el fichero en formato CSV para su lectura.

--- MovieFlix/src/herramientas/CargarCSV.java
+++ MovieFlix/src/herramientas/CargarCSV.java
@@ -1,24 +1,11 @@
 package herramientas;
 
-/*
- * No estoy seguro de que este método funcione, aún no lo he probado.
- */
-
 public class CargarCSV {
 	
-	private String ArchivoCSV = "D:\\ISO-Codes.csv";									// Ruta del archivo CSV
+	private String ArchivoCSV = "Ruta del archivo";									// Ruta del archivo CSV
 	
 	public void LeerCSV() {
 		
-		CSVReader csvReader = new CSVReader(new FileReader(ArchivoCSV));				// Crea una instancia para el lector CSV
-		String[] fila = null;
-		
-		while( (fila = csvReader.readNext()) != null )
-		    System.out.println( fila[0] + " | " + fila[1] + " |  " + fila[2] );			// Modificar
-
-		csvReader.close();
-		
 	}
-	
-	
+
 }
