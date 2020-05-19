6a166da575e50198911ad24ecd8cbb799ba250cf
dvigon <davidvigon@hotmail.es>
2020-05-19 17:20:51 +0200

    Creada la clase Menu

--- MovieFlix/src/menu/Menu.java
+++ MovieFlix/src/menu/Menu.java
@@ -1,7 +1,40 @@
 package menu;
 
+import herramientas.LeerDatos;
+
 // Menu
 
 public class Menu {
+	
+	private int Opcion = LeerDatos.leerInteger("\nIntroduzca una opción: ");	
+	
+	public Menu() {
+		super();
+	}
+	
+	public Menu(int opcion) {
+		super();
+		Opcion = opcion;
+	}
+
+	public int getOpcion() {
+		return Opcion;
+	}
+
+	public void setOpcion(int opcion) {
+		Opcion = opcion;
+	}
+	
+	public void MostrarMenu() {
+		System.out.println("\n *** Bienvenido a MovieFlix *** \n");
+		System.out.println("\n Opción 1:  \n");
+		System.out.println("\n Opción 2:  \n");
+		System.out.println("\n Opción 3:  \n");
+		System.out.println("\n Opción 4:  \n");
+		System.out.println("\n Opción 5:  \n");
+		System.out.println("\n Opción 6:  \n");
+		System.out.println("\n Opción 7:  \n");
+		System.out.println("\n Opción 8:  \n");
+	}
 
 }
