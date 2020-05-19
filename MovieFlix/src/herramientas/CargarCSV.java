64d1ae18c5dddbb302406edf45a68d5705f44b5f
dvigon <davidvigon@hotmail.es>
2020-05-19 17:27:34 +0200

    Creado el método de LeerCSV

--- MovieFlix/src/herramientas/CargarCSV.java
+++ MovieFlix/src/herramientas/CargarCSV.java
@@ -1,5 +1,24 @@
 package herramientas;
 
+/*
+ * No estoy seguro de que este método funcione, aún no lo he probado.
+ */
+
 public class CargarCSV {
-	// Leer CSV
+	
+	private String ArchivoCSV = "D:\\ISO-Codes.csv";									// Ruta del archivo CSV
+	
+	public void LeerCSV() {
+		
+		CSVReader csvReader = new CSVReader(new FileReader(ArchivoCSV));				// Crea una instancia para el lector CSV
+		String[] fila = null;
+		
+		while( (fila = csvReader.readNext()) != null )
+		    System.out.println( fila[0] + " | " + fila[1] + " |  " + fila[2] );			// Modificar
+
+		csvReader.close();
+		
+	}
+	
+	
 }
