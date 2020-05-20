1d28debe610b6ceb664abd4cdc440aafd7b175b0
dvigon <davidvigon@hotmail.es>
2020-05-20 10:48:28 +0200

    Creado el menú para mostrar las opciones.

--- MovieFlix/src/menu/Menu.java
+++ MovieFlix/src/menu/Menu.java
@@ -1,40 +1,40 @@
 package menu;
 
-import herramientas.LeerDatos;
+/**
+ * Mostrar Menú 
+ * @author David Vigón
+ * Fecha: 20-5-2020
+ */
 
-// Menu
+import herramientas.LeerDatos;
 
 public class Menu {
 	
-	private int Opcion = LeerDatos.leerInteger("\nIntroduzca una opción: ");	
-	
-	public Menu() {
-		super();
-	}
-	
-	public Menu(int opcion) {
-		super();
-		Opcion = opcion;
-	}
-
-	public int getOpcion() {
-		return Opcion;
-	}
-
-	public void setOpcion(int opcion) {
-		Opcion = opcion;
-	}
-	
-	public void MostrarMenu() {
-		System.out.println("\n *** Bienvenido a MovieFlix *** \n");
-		System.out.println("\n Opción 1:  \n");
-		System.out.println("\n Opción 2:  \n");
-		System.out.println("\n Opción 3:  \n");
-		System.out.println("\n Opción 4:  \n");
-		System.out.println("\n Opción 5:  \n");
-		System.out.println("\n Opción 6:  \n");
-		System.out.println("\n Opción 7:  \n");
-		System.out.println("\n Opción 8:  \n");
+	public static void MostrarMenu() {
+		
+		System.out.println("Bienvenido a MovieFlix");
+		
+        System.out.println("************************************************");
+		System.out.println("  Opción 1: Mostrar listado de películas completo.");			// Muestra todas las películas de todos los catálogos.
+		System.out.println("  Opción 2: Mostrar listado de películas por catálogo.");		// Pide al usuario el catálogo que desea consultar y muestra el listado de películas que contiene.
+		System.out.println("  Opción 3: Mostrar listado de películas más vistas.");			// Muestra las películas más vistas de cada catálogo.
+		System.out.println("  Opción 4: Mostrar listado de películas más valoradas.");		// Muestra las películas más valoradas de cada catálogo.
+		System.out.println("  Opción 5: Mostrar listado de películas contratado.");			// Muestra las películas que puede ver el usuario con su paquete contratado.
+		System.out.println("  Opción 6: Mostrar listado de clientes completo.");			// Muestra el listado de todos los clientes dados de alta
+		System.out.println("  Opción 7: Mostrar listado de clientes por catálogo.");		// 
+		System.out.println("  Opción 8: Mostrar listado de clientes del catálogo.");		// 
+		System.out.println("  Opción 9: Mostrar listado de catálogos.");					// 
+		System.out.println("  Opción 10: Dar de alta un usuario.");							// 
+		System.out.println("  Opción 11: Dar de baja un usuario.");							// 
+		System.out.println("  Opción 12: Modificar un usuario.");							// 
+		System.out.println("  Opción 13: Dar de alta una película.");						// 
+		System.out.println("  Opción 14: Dar de baja una película.");						// 
+		System.out.println("  Opción 15: Modificar una película.");							// 
+		System.out.println("  Opción 16: Cerrar sesión.");
+        System.out.println("************************************************");
+        
+        LeerDatos.leerByte("Introduce una opción >> ");
+        
 	}
 
 }
