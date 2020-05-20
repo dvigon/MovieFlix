3b2dce6f7ce21d7e101124853adf639054344505
dvigon <davidvigon@hotmail.es>
2020-05-20 11:09:08 +0200

    Modificado el menú.

--- MovieFlix/src/menu/Menu.java
+++ MovieFlix/src/menu/Menu.java
@@ -15,22 +15,24 @@ public class Menu {
         System.out.println("************************************************");
 		System.out.println("*            Bienvenido a MovieFlix            *");
         System.out.println("************************************************");
+        
 		System.out.println("  Opción 1: Mostrar listado de películas completo.");			// Muestra todas las películas de todos los catálogos.
-		System.out.println("  Opción 2: Mostrar listado de películas por catálogo.");		// Pide al usuario el catálogo que desea consultar y muestra el listado de películas que contiene.
+		System.out.println("  Opción 2: Mostrar listado de películas por catálogo.");		// Pide al usuario el nombre del catálogo que desea consultar y muestra su listado de películas.
 		System.out.println("  Opción 3: Mostrar listado de películas más vistas.");			// Muestra las películas más vistas de cada catálogo.
 		System.out.println("  Opción 4: Mostrar listado de películas más valoradas.");		// Muestra las películas más valoradas de cada catálogo.
 		System.out.println("  Opción 5: Mostrar listado de películas contratado.");			// Muestra las películas que puede ver el usuario con su paquete contratado.
-		System.out.println("  Opción 6: Mostrar listado de clientes completo.");			// Muestra el listado de todos los clientes dados de alta
-		System.out.println("  Opción 7: Mostrar listado de clientes por catálogo.");		// 
-		System.out.println("  Opción 8: Mostrar listado de clientes del catálogo.");		// 
-		System.out.println("  Opción 9: Mostrar listado de catálogos.");					// 
-		System.out.println("  Opción 10: Dar de alta un usuario.");							// 
-		System.out.println("  Opción 11: Dar de baja un usuario.");							// 
-		System.out.println("  Opción 12: Modificar un usuario.");							// 
-		System.out.println("  Opción 13: Dar de alta una película.");						// 
-		System.out.println("  Opción 14: Dar de baja una película.");						// 
-		System.out.println("  Opción 15: Modificar una película.");							// 
+		System.out.println("  Opción 6: Mostrar listado de clientes completo.");			// Muestra el listado de todos los clientes dados de alta.
+		System.out.println("  Opción 7: Mostrar listado de clientes por catálogo.");		// Pide al usuario el nombre del catálogo y muestra el listado los clientes que lo tienen contratado.
+		System.out.println("  Opción 8: Mostrar listado de clientes del catálogo.");		// Pide al usuario el nombre del catálogo que desea consultar y muestra su listado de clientes.
+		System.out.println("  Opción 9: Mostrar listado de catálogos.");					// Muestra todos los catálogos disponibles.
+		System.out.println("  Opción 10: Dar de alta un usuario.");							// Pide al usuario el nombre completo, fecha de nacimiento y lugar de residencia del cliente y lo da de alta.
+		System.out.println("  Opción 11: Dar de baja un usuario.");							// Pide al usuario el nombre completo y lo da de baja.
+		System.out.println("  Opción 12: Modificar un usuario.");							// Pide al usuario el nombre completo del usuario a modificar, cuando lo encuentra, pide el nombre completo, fecha de nacimiento, lugar de residencia y el catalogo contratado.
+		System.out.println("  Opción 13: Dar de alta una película.");						// Pide al usuario el nombre de la película, el año de estreno y la categoría, y la da de alta.
+		System.out.println("  Opción 14: Dar de baja una película.");						// Pide al usuario el nombre de la película y la da de baja.
+		System.out.println("  Opción 15: Modificar una película.");							// Pide al usuario el nombre de la película a modificar, cuando lo encuentra, pide el nombre de la película, año de estreno y categoria a la que pertenece.
 		System.out.println("  Opción 16: Cerrar sesión.");
+		
         System.out.println("************************************************");
         
         LeerDatos.leerByte("Introduce una opción >> ");
