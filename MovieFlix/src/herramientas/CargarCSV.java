02525d765f8c5787756c524e92088694990cb231
dvigon <davidvigon@hotmail.es>
2020-05-21 11:49:09 +0200

    Cambio CargarCSV

--- MovieFlix/src/herramientas/CargarCSV.java
+++ MovieFlix/src/herramientas/CargarCSV.java
@@ -20,12 +20,15 @@ public class CargarCSV {
 	
 	ArrayList<String> Titulos = new ArrayList<String>();
 	ArrayList<String> AñoEstreno = new ArrayList<String>();
-	ArrayList<String> Genero = new ArrayList<String>();
+	ArrayList<Integer> Genero = new ArrayList<Integer>();
 	
 	// private Connection conexion = new Conexion().getConnection();							Comentado hasta que funcione la conexión con la BBDD
 	
 	String archivo = "src\\files\\peliculas_cat.txt";
 	String archivo2 = "src\\files\\peliculas_numCat.txt";
+	
+	private String Titulo;
+	private int idGenero, anyoEstreno;
 	
 	private Connection con = new Conexion().getConnection();
 	
@@ -39,9 +42,9 @@ public class CargarCSV {
 
 			stringSeparado = parrafo.split(",");		// Separador
 			
-			Titulos.add( stringSeparado[0] );			// Asigna el primer String a Titulos de películas
-			AñoEstreno.add( stringSeparado[1] );		// Asigna el segundo String a Año de estreno de la películas
-			Genero.add( stringSeparado[2] );			// Asigna el tercer String al Genero de la película
+			Titulo = stringSeparado[0];									// Asigna el primer String a Titulo
+			anyoEstreno = Integer.parseInt( stringSeparado[1] );		// Asigna el segundo String a Año de estreno
+			idGenero = Integer.parseInt( stringSeparado[2] );			// Asigna el tercer String al numero de genero
 			
 		}
 		
@@ -51,22 +54,22 @@ public class CargarCSV {
 
 	public void mostrarTitulos() throws IOException {
 		readFileAsString();
-		System.out.println( "Titulos: "+ Titulos );
+		System.out.println( "Titulos: "+ Titulo );
 	}
 	
 	public void mostrarAñosEstreno() throws IOException  {
-		System.out.println( "Años de estreno: "+ AñoEstreno );
+		System.out.println( "Años de estreno: "+ anyoEstreno );
 	}
 	
 	public void mostrarGeneros() throws IOException  {
-		System.out.println( "Generos: "+ Genero );
+		System.out.println( "Generos: "+ idGenero );
 	}
 
 	public void cargarPeliculas() {
 
 		try (Statement stmt = con.createStatement() ) {
 
-			String query = "INSERT INTO Pelicula ( titulo, anyoEstreno )  VALUES (" + Titulos + "," +  AñoEstreno + ", " + Genero + ")";
+			String query = "INSERT INTO Pelicula ( titulo, anyoEstreno, Genero )  VALUES (" + Titulo + "," +  anyoEstreno + ", " + idGenero + ")";
 			if (stmt.executeUpdate(query) != 1) {
 				System.out.println("Error al insertar las películas");  
 			}
@@ -75,5 +78,5 @@ public class CargarCSV {
 		}
 		
 	}
-	
+
 }
